7972f87d90047fcdf70460d012c17caaab9c8f05
rtiller21@gmail.com <Bamm1234!>
2018-10-03 15:29:55 -0600

    Begin, Title, Import Libraries
    Beginning import libraries ( math, util, io), labled the names.

---
+++ main.java
@@ -0,0 +1,26 @@
+/*AI
+ *By: Robert Tiller, Beau Anderson, Kyle Ungersma, and Jason Armstrong
+ *
+ * DFS
+ * BFS
+ * Greedy Best-first search
+ * A* search
+ * Uniform Cost Search
+ *
+*/
+
+//imported for the use of calculating the power of integers (x^y)
+import java.math.*;
+import java.io.*;
+import java.util.*;
+
+
+
+public class main {
+	/*Depth-First Search Algorithm that takes a Maze (by reference) in as a parameter.
+	 *The Maze class is defined in Maze.java
+         */
+
+    
+
+}
