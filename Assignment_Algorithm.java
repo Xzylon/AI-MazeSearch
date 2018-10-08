be5c6dd12280433d8015d84086bf374ae6682ca0
robert tiller <rtiller21@gmail.com>
2018-10-08 11:35:28 -0600

    BFS moved

---
+++ Assignment_Algorithm.java
@@ -0,0 +1,35 @@
+/*AI
+ *By: Robert Tiller, Beau Anderson, Kyle Ungersma, and Jason Armstrong
+ *
+ * DFS
+ * BFS
+ * Greedy Best-first search
+ * A* search
+ *
+*/
+
+// imported for the use of calculating the power of integers (x^y)
+// import java.math.*;
+// import java.io.*;
+// import java.util.*;
+
+
+
+public class Assignment_Algorithm{
+	/*Depth-First Search Algorithm that takes a Maze (by reference) in as a parameter.
+	 *The Maze class is defined in Maze.java
+         */
+     public static void main(String[] args) {
+        
+         
+    }
+	
+	static void BreadthFirst(Node[][] maze) //Written by Robert Tiller
+    {
+		
+	}
+
+
+    
+
+}
